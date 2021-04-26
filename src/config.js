dcfa2b03ea5200cc46348ab0aa819f172d62aa53
Adam Mayer <r.adammayer@gmail.com>
2021-04-25 20:38:57 -0400

    DATAbase

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: 'postgresql+psycopg2://postgres:@localhost:5432/traits'
+    DATABASE_URL:'postgresql+psycopg2://postgres:@localhost:5432/traits'
   }
