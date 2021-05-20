6f6080c5680e3845fbd59f39020ef87b807f8809
Faizan Mostafa <faizanmustafa324@gmail.com>
2021-05-20 20:58:53 +0500

    knexfile.js updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql+psycopg2://postgres:@localhost:5432/traits'
+    DATABASE_URL: 'postgres://lrelrhxwoskrxa:1578c5e4ad4756996c37cf05bb9229bfd945d3ba5badd9c1ea77770e49c6ddaa@ec2-34-225-167-77.compute-1.amazonaws.com:5432/dd51nkhs6g105a?ssl=true'
   }
