c2c9bf947871e7bcf91a07111795dac64fd7bc18
Adam Mayer <r.adammayer@gmail.com>
2021-05-25 15:04:27 -0400

    env

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,4 +4,7 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL,
+  "database": process.env.DATABASE,
+  "username": process.env.USERNAME,
+  "password": process.env.PASSWORD,
 }
